0e73a8a0b87fd2fb861565abc1c99ab6f8648df3
MhmdFazril <mfazrilmaulana@gmail.com>
2023-07-13 00:20:25 +0700

    perbaikan

--- app/Views/users/dashboard.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend('../templates/navbar'); ?>
-<?= $this->section('content'); ?>
-
-<?= $this->endSection(); ?>
--- app/Views/users/index.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('template/main'); ?>
-<?= $this->section('content'); ?>
-<div class=" bg-slate-100 w-full h-1/2">
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum officiis tempore odio aliquid commodi mollitia optio. Praesentium laudantium accusantium explicabo quibusdam molestiae maiores ipsam accusamus, saepe maxime. Voluptatem, repellat ipsa!</p>
-</div>
-
-<?= $this->endSection(); ?>
